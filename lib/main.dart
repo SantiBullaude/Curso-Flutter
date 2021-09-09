a21693bf0e579ac95d30ce7f72ed69caa0d9ca10
SantiagoBullaude <santiagobullaude@gmail.com>
2021-09-09 17:57:17 -0300

    agregue?

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+
+
+  import 'package:flutter/cupertino.dart';
+  import 'package:flutter/material.dart';
+
+  // Nosotros Escribimos
+  import 'src/app.dart';
+void main (){
+
+    runApp(new MyApp());
+
+
+  }
+
+
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+  class HomePage extends StatelessWidget{
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Titulo'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Numero de click:', style: TextStyle(fontSize: 25)),
+            Text('0', style: TextStyle(fontSize: 25)),
+          ],
+        )
+
+      )
+    );
+  }
+  }
